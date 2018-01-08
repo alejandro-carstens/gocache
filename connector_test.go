7a78b87627d945b92b7daa06ae63a4d60afedfa0
Alejandro <acarste@okstate.edu>
2018-01-08 03:44:26 +0000

    refactoring connector tests.

---
+++ connector_test.go
@@ -0,0 +1,37 @@
+package cache
+
+import (
+	"testing"
+)
+
+func TestMemcacheConnector(t *testing.T) {
+	memcacheConnector := new(MemcacheConnector)
+
+	memcacheStore, err := memcacheConnector.Connect(memcacheStore())
+
+	if err != nil {
+		panic(err)
+	}
+
+	_, ok := memcacheStore.(StoreInterface)
+
+	if !ok {
+		t.Error("Expected StoreInterface got", memcacheStore)
+	}
+}
+
+func TestRedisConnection(t *testing.T) {
+	redisConnector := new(RedisConnector)
+
+	redisStore, err := redisConnector.Connect(redisStore())
+
+	if err != nil {
+		panic(err)
+	}
+
+	_, ok := redisStore.(StoreInterface)
+
+	if !ok {
+		t.Error("Expected StoreInterface got", redisStore)
+	}
+}
