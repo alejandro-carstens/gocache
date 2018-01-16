5576d710880e443cf0c99ecd704df303df0c3f83
Alejandro <acarste@okstate.edu>
2018-01-16 03:38:46 +0000

    adding comments.

--- helpers.go
+++ helpers.go
@@ -7,24 +7,28 @@ import (
 	"strconv"
 )
 
+// Encode encodes json.Marshal to a string
 func Encode(item interface{}) (string, error) {
 	value, err := json.Marshal(item)
 
 	return string(value), err
 }
 
+// SimpleDecode decodes json.Unmarshal to a string
 func SimpleDecode(value string) (string, error) {
 	err := json.Unmarshal([]byte(value), &value)
 
 	return string(value), err
 }
 
+// Decode performs a json.Unmarshal
 func Decode(value string, entity interface{}) (interface{}, error) {
 	err := json.Unmarshal([]byte(value), &entity)
 
 	return entity, err
 }
 
+// IsNumeric check if the provided value is numeric
 func IsNumeric(s interface{}) bool {
 	switch s.(type) {
 	case int:
@@ -40,6 +44,7 @@ func IsNumeric(s interface{}) bool {
 	}
 }
 
+// GetTaggedManyKey returns a tagged many key
 func GetTaggedManyKey(prefix string, key string) string {
 	count := len(prefix) + 41
 
@@ -61,16 +66,19 @@ func GetTaggedManyKey(prefix string, key string) string {
 	return subs[1]
 }
 
+// IsStringNumeric checks if a string is numeric
 func IsStringNumeric(value string) bool {
 	_, err := strconv.ParseFloat(value, 64)
 
 	return err == nil
 }
 
+// StringToFloat64 converts a string to float64
 func StringToFloat64(value string) (float64, error) {
 	return strconv.ParseFloat(value, 64)
 }
 
+// Checks if the provided value can be truncated to an int
 func IsFloat(value float64) bool {
 	return value != math.Trunc(value)
 }
